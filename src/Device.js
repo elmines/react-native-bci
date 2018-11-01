b5eb9182bfea1c7b5a25e3320a043a80f1cfe45c
Ethan Mines <elmines325@gmail.com>
2018-10-31 20:47:59 -0500

    Write code that passes Flow

---
+++ src/Device.js
@@ -0,0 +1,26 @@
+//@flow
+
+import {Observable} from "rxjs";
+
+export type ConnectStatus = "Disconnected" | "Connecting" | "Connected" | "Disconnecting";
+
+//Matches the @neurosity/pipes
+export type DataPacket = {
+	data: Array<Number>,
+	timestamp: Date,
+	info?: {
+		samplingRate?: number,
+		channelNames?: Array<string>
+	}
+}
+
+export class DeviceManager
+{
+	getChannelNames(): Array<string> {return []};
+
+	dataSubject(): Observable<DataPacket>{return new Observable<DataPacket>();}
+
+	connect(identifier: string): void {};
+  startListening(): void {};
+	stopListening(): void {}
+}
